76241fc9c1cf3d7a38329fc86a2b939e5eaf93d6
Dan Potepa <dan.potepa@lush.co.uk>
2018-08-22 09:56:45 +0100

    initial

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"github.com/golangdorset/simpleratelimiting/utils"
+	"fmt"
+	"time"
+)
+
+func main(){
+	one()
+	//two()
+	//three()
+}
+
+// ranging over the channel you can see that we print out the int as soon as it is written in by the feeder.
+func one(){
+	requests := make(chan int, 100)
+
+	go utils.Feed(requests)
+
+	for i := range requests {
+		fmt.Println("\t\t\t\t\tout:", i)
+	}
+}
+
+// using a ticker that puts the time on the channel every 1 second, we can delay the reading from the requests queue
+// as shown by the rate of requests going in, and being read out
+func two(){
+	requests := make(chan int, 100)
+
+	go utils.Feed(requests)
+
+	drip := time.Tick(time.Second)
+
+	for i := range requests {
+		<- drip
+		fmt.Println("\t\t\t\t\tout:", i, utils.PrintNow())
+	}
+}
+
+
+// feed2 randomly adds up to 20 ints at random intervals, then will sleep for 20 seconds before continuing, this
+// allows the "buffer" to re-fill.
+func three(){
+	requests := make(chan int, 100)
+
+	go utils.Feed2(requests)
+
+	drip := time.Tick(time.Second)
+
+	//type of this channel is not important
+	burstyLimiter := make(chan bool, 5)
+
+	// pre-fill the buffer channel so that the first 5 requests can be processes without waiting for the "tick"
+	for i:=0;i<5;i++ {
+		burstyLimiter <- true
+	}
+
+
+	// in the background, every second (using the drip feed ticker from before) add a "token" to the bursty limiter
+	// if no requests come in for a period of time, then this "buffer" will fill back to 5
+	// the next 5 requests will be able to read from the channel and be processed straight away
+	go func() {
+		for {
+			<-drip
+			burstyLimiter <- true
+		}
+	}()
+
+	// print the status of the request queue and size of the "buffer" limiter
+	go utils.PrintChans(requests, burstyLimiter)
+
+
+
+	for i := range requests {
+		<- burstyLimiter
+		fmt.Println("\t\t\t\t\tout:", i, utils.PrintNow())
+	}
+}
---
+++ utils/utils.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"time"
+		"math/rand"
+	"fmt"
+)
+
+// simulate randomish requests by pushing ints into the provided channel
+func Feed(f chan int){
+	count := 0
+	for {
+		randSleep := rand.Intn(100)
+		time.Sleep(time.Millisecond * time.Duration(randSleep * 10))
+
+		fmt.Println("in:", count)
+		f <- count
+		count++
+	}
+}
+
+// simulate randomish requests by pushing ints into the provided channel but pause after 25 request
+// to allow the demo "buffer" to refill
+func Feed2(f chan int){
+	for i:=0;i<20;i++{
+		randSleep := rand.Intn(100)
+		time.Sleep(time.Millisecond * time.Duration(randSleep * 10))
+
+		fmt.Println("in:", i)
+		f <- i
+	}
+	time.Sleep(time.Second * 20)
+	for i:=25;i<100;i++{
+		randSleep := rand.Intn(100)
+		time.Sleep(time.Millisecond * time.Duration(randSleep * 10))
+
+		fmt.Println("in:", i)
+		f <- i
+	}
+}
+
+// print the time in a simple format
+func PrintNow() string{
+	return time.Now().Format("15:04:05")
+}
+
+// reports the size of the requests queue and burst buffer, and badly pad it to the right of the terminal
+func PrintChans(queue chan int, buffer chan bool){
+	for {
+		fmt.Printf("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\trequest queue: %v, buffer: %v\n", len(queue), len(buffer))
+		time.Sleep(time.Millisecond * 500)
+	}
+}
